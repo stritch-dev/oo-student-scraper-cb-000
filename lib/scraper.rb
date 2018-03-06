7a3c30393d1006747aaa1d59e6c4d9bb5bbd7526
stritch.dev <stritch.dev@gmail.com>
2018-03-06 13:43:58 -0600

    passed all tests

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,39 @@
 require 'open-uri'
-require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open index_url)
+    page.css("div[class='student-card']").collect do |card|
+     {
+       :name => card.css("a div h4").text.gsub('\n',''),
+       :location => card.css("a div p").text.gsub('\n',''),
+       :profile_url => card.css("a")[0]['href'] 
+      } 
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    page = Nokogiri::HTML(open profile_url)
+    output = Hash.new
+    output[:bio] = page.css('div.bio-content p').text
+    output[:profile_quote] = page.css('div.profile-quote').text
+    page.css('div.social-icon-container a').collect do |anchor|
+      anchor.collect do |url|
+        address = url[1]
+        if address.include? 'twitter'
+            output[:twitter] = address
+          elsif address.include? 'linkedin'
+            output[:linkedin] = address
+          elsif address.include? 'github.com'
+            output[:github] = address
+          else 
+            output[:blog] = address
+        end
+      end
+    end
+    output
   end
-
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +6,24 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    # source https://stackoverflow.com/questions/13303401/quickly-setting-instance-variables-with-options-hash-in-ruby
+    student_hash.each { |k,v| instance_variable_set("@#{k}", v) }
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
+    students_array.each do |student|
+     Student.new(student)
+    end
     
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each { |k,v| instance_variable_set("@#{k}", v) }
   end
 
   def self.all
-    
+    @@all
   end
 end
 
